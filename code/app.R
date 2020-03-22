8135bb35a7f226791edffdd8351de0958dfc27e4
Nikolaj <36231472+NikolajT@users.noreply.github.com>
2020-03-22 15:46:00 +0100

    Update with one additional curve and input of death rate and rel_risk

--- code/app.R
+++ code/app.R
@@ -634,11 +634,17 @@ ui <- dashboardPage(
                 choices = c("Original scale" = "unscaled", "Logarithmic scale" = "log"),
                 selected="log"
               ),
+              radioButtons(
+                "wvv.compare_ouput",
+                "Compare estimator to",
+                choices = c("Deaths" = "deaths", "Confirmed cases" = "confirmed_cases"),
+                selected="confirmed_cases"
+              ),
               
               selectInput(
                 "wvv.countries",
                 "Countries",
-                choices = countries,
+                choices = data$Country.Region,
                 selected = c("Denmark", "Italy", "United Kingdom", "US", "Spain"),
                 multiple = T
               ), 
@@ -647,6 +653,16 @@ ui <- dashboardPage(
                 "Days from infection to death",
                 value = 14, 
                 min = 1
+              ),
+              textInput(
+                "wvv.death_rate", 
+                "Death rate (comma-separated)", 
+                value=c("0, 0, 0, 0.0011, 0.0008, 0.0042, 0.0152, 0.0628, 0.1024")
+              ),
+              textInput(
+                "wvv.rel_risk", 
+                "Relative risk (comma-separated)", 
+                value=c("0, 0, 0, 0.0014,0.004,0.013,0.065,0.274,0.641")
               )
             ),
             
@@ -714,7 +730,87 @@ server <- function(input, output) {
         "PercentageOfPopulation" = (Cases / Population) * 100
       ) %>% ungroup
   })
-
+  
+  make.wvv.data <- reactive({
+    ########################################
+    # WVV data ----------------
+    ## Read data
+    demographics <- read.csv("../wirvsvirus/demographics.csv", sep=";", stringsAsFactors=FALSE, dec=",")
+    deathdata <- read.csv("../csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv", header=TRUE, stringsAsFactors=FALSE, dec=",")
+    confirmeddata <- read.csv("../csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv", header=TRUE, stringsAsFactors=FALSE, dec=",")
+    demographics <- demographics[order(demographics$Country),]
+    deathdata <- deathdata[order(deathdata$Country.Region),]
+    
+    ## Prepare deathdata
+    drops <- c("Lat", "Long", "Province.State")
+    deathdata <- deathdata[,!(names(deathdata) %in% drops)]
+    deathdata <- aggregate(. ~ Country.Region, data = deathdata, FUN = sum)
+    dates = as.Date(substring(colnames(deathdata)[2:ncol(deathdata)], 2), format = "%m.%d.%y")
+    
+    confirmeddata <- confirmeddata[,!(names(confirmeddata) %in% drops)]
+    confirmeddata <- aggregate(. ~ Country.Region, data = confirmeddata, FUN = sum)
+    
+    demographics <- subset(demographics, select=c(-Total.population))
+    colnames(demographics)[2:ncol(demographics)] = paste0(10*(0:8), rep("-", 8), 9+10*(0:8))
+    
+    ## Align data
+    countries <- intersect(demographics$Country, deathdata$Country.Region)
+    deathdata = subset(deathdata, Country.Region %in% countries)
+    confirmeddata = subset(confirmeddata, Country.Region %in% countries)
+    demographics = subset(demographics, Country %in% countries)
+    
+    death_mat = as.matrix(subset(deathdata, select=c(-Country.Region)))
+    demographics = as.matrix(subset(demographics, select=c(-Country)))
+    
+    ## These two parameters need to be adjusted
+    death.rate =  as.numeric(unlist(strsplit(input$wvv.death_rate,",")))
+    relative.death.risk = as.numeric(unlist(strsplit(input$wvv.rel_risk,",")))
+    
+    # numbers from south korea
+    # https://en.wikipedia.org/wiki/Coronavirus_disease_2019#Prognosis
+    ## Actual relevant computation
+    activedata <- matrix(NA, nrow(death_mat), ncol(death_mat))
+    for(i in 1:nrow(activedata)){
+      for(j in 1:ncol(activedata)){
+        demo_adjusted_risk <- relative.death.risk * demographics[i,]
+        est.num.death <- demo_adjusted_risk*death_mat[i, j]/sum(demo_adjusted_risk)
+        activedata[i, j] <- sum(est.num.death/death.rate, na.rm=TRUE)
+      }
+    }
+    activedata = data.frame(activedata)
+    colnames(activedata) = dates
+    activedata$Country = countries
+    
+    activedata <- melt(
+      activedata,
+      id.vars = "Country",
+      variable.name = "Date",
+      value.name = "Cases"
+    )
+    activedata$Date = as.Date(activedata$Date)
+    
+    confirmeddata <- melt(
+      confirmeddata,
+      id.vars = "Country.Region",
+      variable.name = "Date",
+      value.name = "ConfirmedCases"
+    )
+    confirmeddata$Date = as.Date(substring(confirmeddata$Date, 2), format = "%m.%d.%y")
+    
+    
+    deathdata <- melt(
+      deathdata,
+      id.vars = "Country.Region", 
+      variable.name = "Date", 
+      value.name = "Deaths"
+    )
+    deathdata$Date = as.Date(substring(deathdata$Date, 2), format = "%m.%d.%y")
+    
+    wvv.data = merge(activedata, deathdata, by.x=c("Country", "Date"), by.y=c("Country.Region", "Date"))
+    wvv.data = merge(wvv.data, confirmeddata, by.x=c("Country", "Date"), by.y=c("Country.Region", "Date"))
+    wvv.data
+  })
+  
   output$JH_data_lastupdate <- renderText({
     paste(
       "John Hopkins data was last updated at:",
@@ -869,6 +965,30 @@ server <- function(input, output) {
   })
   
   output$wirvsvirus <- renderPlotly({
+    wvv.data = make.wvv.data()
+    make_estimate_plot = function(input){
+      if(input$wvv.compare_ouput == "confirmed_cases"){
+        firstDate = min(subset(wvv.data, (ConfirmedCases != 0 & Country %in% input$wvv.countries))$Date)  
+      } else {
+        firstDate = min(subset(wvv.data, (Deaths != 0 & Country %in% input$wvv.countries))$Date)  
+      }
+      
+      p = ggplot(subset(wvv.data, (Country %in% input$wvv.countries & Date >= firstDate)), 
+                 aes(colour=Country, group=Country)) + 
+        scale_x_date(breaks = date_breaks("week"), date_labels = "%b %d")
+      
+      if(input$wvv.compare_ouput == "confirmed_cases"){
+        p = p + geom_line(aes(x = Date, y = ConfirmedCases))
+      } else {
+        p = p + geom_line(aes(x = Date, y = Deaths))
+      }
+      p = p + geom_line(aes(x = Date - input$wvv.death_delay, y = Cases), linetype="dashed")
+      
+      if (input$wvv.log == "log") {
+        p <- p + scale_y_log10()
+      }
+      return(p)
+    }
     p = make_estimate_plot(input)
     ggplotly(p)
     
