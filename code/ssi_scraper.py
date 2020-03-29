f014d40e5b78e9a29c2d4163e5680fc1ff4b49fc
Phillip Bredahl Mogensen <pbm@math.ku.dk>
2020-03-29 14:08:17 +0200

    Cleaned up code

--- code/ssi_scraper.py
+++ code/ssi_scraper.py
@@ -4,42 +4,50 @@ from urllib.error import HTTPError
 import tabula
 
 today = date.today().strftime("%d%m%Y")
+ending = "-f67s"
+
 
 def get_timeseries(date):
-    file = "https://files.ssi.dk/COVID19-overvaagningsrapport-" + date
-    # file = "https://files.ssi.dk/COVID19-overvaagningsrapport-" + "24032020"
+    file = "https://files.ssi.dk/COVID19-overvaagningsrapport-" + date + ending
 
     try:
-        tables = tabula.read_pdf(file, pages='all', multiple_tables=True, stream=True)
+        tables = tabula.read_pdf(
+            file, pages=8,
+            multiple_tables=True,
+            area=(127.34, 70.94, 324.95 + 127.34, 396.55 + 70.94),
+            stream=True)
     except HTTPError:
         return print("No data found for date " + date)
+        return None
 
-    vals = ['Prøvedato' in tables[i].values.flatten() for i in range(len(tables))]
+    vals = ['Prøvedato' in tables[i].values.flatten()
+            for i in range(len(tables))]
     correct_table = [i for i, n in enumerate(vals) if n][0]
 
-    if date > "23032020":
-        startval = 2
-    else:
-        startval = 3
+    startval = 2
 
     clean_df1 = tables[correct_table]
     a, b = clean_df1.shape
-    clean_df1 = clean_df1.iloc[startval:(a - 1), :].rename(columns={
+    clean_df1 = clean_df1.iloc[startval:a, :].rename(columns={
         "Unnamed: 0": "Date",
         "Laboratorie-bekræftede": "Lab confirmed cases",
         "Antal testede": "Tested",
         "Andel positive": "Rate"})
 
-    outfile = 'data/ssi.csv'
+    outfile = 'code/data/ssi.csv'
     clean_df1.to_csv(outfile)
     return print("Successfully exported file to path " + outfile)
 
 
 def get_AllTables(date):
-    file = "https://files.ssi.dk/COVID19-overvaagningsrapport-" + date
+    file = "https://files.ssi.dk/COVID19-overvaagningsrapport-" + date + ending
 
     try:
-        tables = tabula.read_pdf(file, pages='all', multiple_tables=True, stream=True)
+        tables = tabula.read_pdf(
+            file,
+            pages='all',
+            multiple_tables=True,
+            stream=True)
     except HTTPError:
         print("No data found for date " + date)
         return None
@@ -48,52 +56,52 @@ def get_AllTables(date):
 
 
 def get_AgeGroups(date):
-    tables = get_AllTables(date)
+    file = "https://files.ssi.dk/COVID19-overvaagningsrapport-" + date + ending
 
-    if tables is None:
+    top = 432.82
+    left = 71.6
+    width = 423.18
+    height = 227.72
+
+    try:
+        tables = tabula.read_pdf(
+            file, pages=10,
+            multiple_tables=False,
+            area=(top, left, top + height, left + width),
+            stream=True)[0]
+    except HTTPError:
         print("No data found for date " + date)
         return None
-    else:
-        vals = ['Aldersgrupper' in tables[i].columns for i in range(len(tables))]
-        agegroups = [i for i, n in enumerate(vals) if n][0]
 
-    return tables[agegroups].drop([0, tables[agegroups].shape[0]-1])
+    a, b = tables.shape
+    clean_df = tables.iloc[1:a, :]
 
-###### Get time series:
-get_timeseries("24032020")
-tst = get_AgeGroups("24032020")
+    clean_df.iloc[:, 3] = clean_df.iloc[:, 3].str.replace(",", ".")
 
-###### Get age data:
-avail_dates = ['12032020', '13032020', '16032020','17032020', '18032020', '19032020', '20032020', '21032020', '22032020']
-for date in avail_dates:
-    datatable = get_AgeGroups(date)
-    outfile = 'data/ssi_agegroups/data_' + date + '.csv'
-    datatable.to_csv(outfile)
-    print('Wrote to file ' + outfile)
+    for i in range(a - 1):
+        if clean_df.iloc[i, 2] < 10:
+            clean_df.iloc[i, 2] = clean_df.iloc[i, 2] * 1000
 
-###### Testing data
-tables = tabula.read_pdf(file, pages=2, multiple_tables=True, stream=True)
+    if "Andel positive" in clean_df.columns.values:
+        clean_df = clean_df.rename(
+            columns={"Andel positive": "Procent positiv"})
 
-clean_df1 = tables[0]
-a, b = clean_df1.shape
-clean_df1 = clean_df1.iloc[3:(a - 1), :].rename(columns={
-    "Unnamed: 0": "Date", "Laboratorie-": "Lab confirmed cases", "Unnamed: 1": "Tested", "Unnamed: 2": "Rate"})
-
-outfile = 'code/data/ssi.csv'
-clean_df1.to_csv(outfile)
-
-clean_df2 = tables[1]
-a, b = clean_df2.shape
-clean_df2 = clean_df2.iloc[4:(a-1), :].rename(columns={"Unnamed: 0": "Region", "Unnamed: 1": "Intensive care"})
-
-outfile2 = 'code/data/ssi2.csv'
-clean_df2.to_csv(outfile2)
+    return clean_df
 
 
-###### Map data
-tables = tabula.read_pdf(file, pages=3, multiple_tables=False, stream=True)
-clean_df3 = tables[0].rename(columns={"Laboratoriebekræftede": "Lab confirmed cases"})
-a, b = clean_df3.shape
-clean_df3 = clean_df3.iloc[range(1, a-1), :]
+# Get time series:
+get_timeseries(today)
 
-clean_df3.to_csv('code/data/ssi_map.csv')
+# Get age data
+_tmp = get_AgeGroups(today)
+_tmp.to_csv("code/data/ssi_agegroups/data_" + today + ".csv")
+"Andel positive" in _tmp.columns.values
+
+# Get all age data: #### Depreceated
+# avail_dates = ['12032020', '13032020', '16032020', '17032020',
+#                '18032020', '19032020', '20032020', '21032020', '22032020']
+# for date in avail_dates:
+#     datatable = get_AgeGroups(date)
+#     outfile = 'data/ssi_agegroups/data_' + date + '.csv'
+#     datatable.to_csv(outfile)
+#     print('Wrote to file ' + outfile)
