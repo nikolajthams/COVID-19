e62144dcfe4e69af21bc05cea3da6aa5e02ca183
Nikolaj <36231472+NikolajT@users.noreply.github.com>
2020-03-30 09:45:21 +0200

    Rename folder and update wirvsvirus name

--- app.R
+++ app.R
@@ -499,7 +499,7 @@ ui <- dashboardPage(
                        text = "Danish data on COVID19 tests", tabName = "ssidat", icon = icon("bar-chart-o")
                      ),
                      menuItem(
-                       text = "Estimated number of dark cases", tabName = "wirvsvirus", icon = icon("file-alt")
+                       text = "Estimated number of total infections (including unreported)", tabName = "wirvsvirus", icon = icon("file-alt")
                      ),
                      menuItem(
                        text = "Exponential growth models", tabName = "expmod_head", icon = icon("dashboard"),
